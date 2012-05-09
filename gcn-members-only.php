b71fb5d42d3f28a29615ba71c81ca77ecf85601d
Harry Metcalfe <harry@thedextrousweb.com>
2012-05-09 12:03:21 +0100

    Added a note about the settings page

--- gcn-members-only.php
+++ gcn-members-only.php
@@ -2,7 +2,7 @@
 /*
  * Plugin Name: GCN Members Only
  * Plugin URI: http://dxw.com
- * Description: Forces users to log in
+ * Description: Forces users to log in. Visit the settings page for options.
  * Version: 1.0
  * Author: dxw
  * Author URI: http://dxw.com
