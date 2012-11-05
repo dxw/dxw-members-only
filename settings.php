bd39890807d4676b3746e17cc1d7f3533a8d4699
Tom Adams <tom@dxw.com>
2012-11-05 09:18:09 -0500

    Add option to settings page

--- settings.php
+++ settings.php
@@ -1,7 +1,7 @@
 <?php
 
 $ms = new MetaSettings(__FILE__, 'new_members_only');
-$ms->add_settings(__('New Members Only'), array('list_type', 'list_content', 'redirect'), 'new_members_only_options_page');
+$ms->add_settings(__('New Members Only'), array('list_type', 'list_content', 'redirect', 'upload_default'), 'new_members_only_options_page');
 
 function new_members_only_options_page() {
 ?>
@@ -42,7 +42,7 @@ function new_members_only_options_page() {
     </table>
 
     <h3><?php _e('Redirection') ?></h3>
-    <p>In both the following options, <code>%return_path%</code> will be converted to the URL that was originally visited. i.e. <code>/wp-login.php?redirect_to=%return_path%</code></p>
+    <p><?php _e('In both the following options, <code>%return_path%</code> will be converted to the URL that was originally visited. i.e. <code>/wp-login.php?redirect_to=%return_path%</code>') ?></p>
 
     <table class="form-table">
 
@@ -58,6 +58,30 @@ function new_members_only_options_page() {
         <td>
           <input type="text" name="new_members_only_redirect_root" id="new_members_only_redirect_root" value="<?php form_option('new_members_only_redirect_root') ?>" class="regular-text">
           <span class="description"><?php _e('Only applies in whitelist mode or if "/" is blacklisted.') ?></span>
+        </td>
+      </tr>
+
+    </table>
+
+    <h3><?php _e('File uploads') ?></h3>
+
+    <table class="form-table">
+
+      <tr valign="top">
+        <th scope="row"><?php _e('New uploads by default are') ?></th>
+        <td>
+          <fieldset>
+            <label>
+              <input type="radio" name="new_members_only_upload_default" value="true" <?php echo get_option('new_members_only_upload_default') === 'true' ? 'checked' : '' ?>>
+              <?php _e('Added to list') ?>
+            </label>
+            <br>
+            <label>
+              <input type="radio" name="new_members_only_upload_default" value="false" <?php echo get_option('new_members_only_upload_default') === 'true' ? '' : 'checked' ?>>
+              <?php _e('Not added to list') ?>
+            </label>
+          </fieldset>
+          <span class="description"><?php _e('You can change this on a per-file basis when uploading') ?></span>
         </td>
       </tr>
 
