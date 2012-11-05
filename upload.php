734edc10745b5ba62d3c509c68fdceecb87ebe00
Tom Adams <tom@dxw.com>
2012-11-05 16:16:04 -0500

    Only show the checkbox when file is freshly uploaded

--- upload.php
+++ upload.php
@@ -1,24 +1,18 @@
 <?php
 
-function clog($x) {
-  echo '<script>console.log(';
-  echo json_encode($x);
-  echo ')</script>';
-}
-
 add_filter('attachment_fields_to_edit', function ($fields, $post) {
-  clog('attachment_fields_to_edit');
-  clog($fields);
-  clog($post);
 
-  $list_type = get_option('new_members_only_list_type');
-  $upload_default = get_option('new_members_only_upload_default');
+  if (realpath($_SERVER['SCRIPT_FILENAME']) === ABSPATH.'wp-admin/async-upload.php') {
+    $list_type = get_option('new_members_only_list_type');
+    $upload_default = get_option('new_members_only_upload_default');
 
-  $fields['nmo_add_to_list'] = array(
-    'input' => 'html',
-    'label' => $list_type === 'blacklist' ? _('Add to blacklist') : _('Add to whitelist'),
-    'html' => '<input type="checkbox" name="attachments['.$post->ID.'][nmo_add_to_list]" '.($upload_default==='true'?'checked':'').'>',
-  );
+    $fields['nmo_add_to_list'] = array(
+      'input' => 'html',
+      'label' => $list_type === 'blacklist' ? _('Add to blacklist') : _('Add to whitelist'),
+      'html' => '<input type="checkbox" name="attachments['.$post->ID.'][nmo_add_to_list]" '.($upload_default==='true'?'checked':'').'>',
+    );
+  }
+
   return $fields;
 }, 10, 2);
 
@@ -30,4 +24,6 @@ add_filter('attachment_fields_to_save', function ($post, $attachment) {
     $list_content .= "\n" . $url;
     update_option('new_members_only_list_content', $list_content);
   }
+
+  return $post;
 }, 10, 2);
