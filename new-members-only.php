e0e0a22be532efbb0c3f974c68121e0f6415e871
Tom Adams <tom@dxw.com>
2015-09-08 15:35:43 -0400

    Bump the version number

--- new-members-only.php
+++ new-members-only.php
@@ -3,7 +3,7 @@
  * Plugin Name: New Members Only
  * Plugin URI: http://dxw.com
  * Description: Make your WordPress site visible to signed-in users only with the added ability to whitelist specific content for access by all users.
- * Version: 1.0
+ * Version: 3.0.0
  * Author: dxw
  * Author URI: http://dxw.com
  * Text Domain: membersonly
