d971d53276ac7fc337e06b49ff5ec0bfaec76e2f
Sarah Mount <sarah.mount@dxw.com>
2024-12-06 17:47:25 +0000

    Release v4.3.3

--- dxw-members-only.php
+++ dxw-members-only.php
@@ -3,7 +3,7 @@
  * Plugin Name: dxw Members Only
  * Plugin URI: http://dxw.com
  * Description: Make your WordPress site visible to signed-in users only with the added ability to allow-list specific content for access by all users.
- * Version: 4.3.2
+ * Version: 4.3.3
  * Author: dxw
  * Author URI: http://dxw.com
  * Text Domain: dxwmembersonly
