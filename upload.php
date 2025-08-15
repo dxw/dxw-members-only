ae8a203f1dc83e8189ef5ca585106e14b39e5a48
Rob Skilling <rob@dxw.com>
2025-08-15 11:53:45 +0100

    Make upload.php Psalm-compliant

--- upload.php
+++ upload.php
@@ -1,8 +1,8 @@
 <?php
 
 add_filter('attachment_fields_to_edit', function ($fields, $post) {
-	if (realpath($_SERVER['SCRIPT_FILENAME']) === ABSPATH.'wp-admin/async-upload.php') {
-		$upload_default = get_option('dxw_members_only_upload_default');
+	if (isset($_SERVER['SCRIPT_FILENAME']) && realpath($_SERVER['SCRIPT_FILENAME']) === ABSPATH.'wp-admin/async-upload.php') {
+		$upload_default = (string) get_option('dxw_members_only_upload_default');
 
 		$fields['dmo_add_to_list'] = [
 			'input' => 'html',
@@ -16,9 +16,9 @@ add_filter('attachment_fields_to_edit', function ($fields, $post) {
 
 add_filter('attachment_fields_to_save', function ($post, $attachment) {
 	if (isset($attachment['dmo_add_to_list']) && $attachment['dmo_add_to_list']) {
-		$url = parse_url($attachment['url'], PHP_URL_PATH);
+		$url = parse_url((string) $attachment['url'], PHP_URL_PATH);
 
-		$list_content = get_option('dxw_members_only_list_content');
+		$list_content = (string) get_option('dxw_members_only_list_content');
 		$list_content .= "\n" . $url;
 		update_option('dxw_members_only_list_content', $list_content);
 	}
