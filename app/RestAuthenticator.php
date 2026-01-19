da674ca26837f557404c787ae8af8a47c33c1232
patdel0 <zefiru@protonmail.com>
2026-01-19 15:44:19 +0000

    Register `rest_authentication_errors` callback

--- app/RestAuthenticator.php
+++ app/RestAuthenticator.php
@@ -6,5 +6,15 @@ class RestAuthenticator implements \Dxw\Iguana\Registerable
 {
 	public function register(): void
 	{
+		add_filter('rest_authentication_errors', [$this, 'authenticate'], 10, 1);
+	}
+
+	/**
+	 * @param \WP_Error|null|true $errors
+	 * @return \WP_Error|null|true
+	 */
+	public function authenticate($errors)
+	{
+		return null;
 	}
 }
--- spec/rest_authenticator.spec.php
+++ spec/rest_authenticator.spec.php
@@ -8,4 +8,14 @@ describe(Dxw\MembersOnly\RestAuthenticator::class, function () {
 	it('implements the registerable interface', function () {
 		expect($this->restAuthenticator)->toBeAnInstanceOf(\Dxw\Iguana\Registerable::class);
 	});
+
+	describe('->register()', function () {
+		it('adds the filter', function () {
+			allow('add_filter')->toBeCalled();
+
+			expect('add_filter')->toBeCalled()->once()->with('rest_authentication_errors', [$this->restAuthenticator, 'authenticate'], 10, 1);
+
+			$this->restAuthenticator->register();
+		});
+	});
 });
